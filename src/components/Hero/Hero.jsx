3d0d159043df388651eb40129d88d38c7e5ed823
unknown <dannyan29@gmail.com>
2020-11-17 00:23:43 -0500

    changecolor'

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,9 +27,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Danny An'}</span>
+            <span className="text-color-main">{name || 'Danny Chanhwi An'}</span>
             <br />
-            {subtitle || "I'm the Back End Developer."}
+            {subtitle || "I'm the Back End Developer!"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
